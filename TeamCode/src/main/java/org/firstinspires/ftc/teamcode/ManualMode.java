12dedefac7fca4737fec10dfe309082779e30727
222165 <222165@wsr.com>
2019-08-27 20:11:00 -0500

    Added basic automonous/manual mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by shell on 08/27/2019.
+ */
+
+@TeleOp
+
+public class ManualMode extends LinearOpMode {
+
+    // Declare motors/servos/variables
+	private DcMotor myMotor;
+
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize motors/servos
+        myMotor = hardwareMap.get(DcMotor.class, "myMotor");
+
+        // Set status
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        // Do this while program is running
+        while (opModeIsActive()) {
+
+            if (this.gamepad1.x) {
+            } else if (this.gamepad1.b) {
+            } else if (this.gamepad1.y) {
+            } else if (this.gamepad1.a) {
+            }
+
+            if (this.gamepad1.dpad_left) {
+            } else if (this.gamepad1.dpad_right) {
+            }
+
+            if (this.gamepad1.right_trigger > 0.5) { // When right trigger is clicked
+            } else if (this.gamepad1.left_trigger > 0.5) { // When left trigger is clicked
+            }
+
+            if (this.gamepad1.dpad_up == this.gamepad1.dpad_down) {
+            } else if (this.gamepad1.dpad_up) {
+            } else if (this.gamepad1.dpad_down) {
+            }
+
+            // Display information about the motors
+            telemetry.addData("MyMotor power is ", myMotor.getPower());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+    }
+}
