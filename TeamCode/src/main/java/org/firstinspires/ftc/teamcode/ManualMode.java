c0db5a7e5cc56b9a0b7b5e320759264bda357ec3
ShellBots <wsr.shell.bots@gmail.com>
2019-09-24 19:46:30 -0500

    Disabled unused linear manual mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,6 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  */
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Basic Linear Opmode", group="Manual")
+@Disabled
 public class ManualMode extends LinearOpMode {
 
     // Declare motors/servos/variables
